e49eaf7c352adae16bccdc43ad3d13054d2f4fdb
Yasuhara Kenta <0714.yasuk@gmail.com>
2021-09-22 09:52:12 +0900

    [Add]generateコマンド時の設定

--- config/application.rb
+++ config/application.rb
@@ -22,12 +22,12 @@ module RunteqNormal
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
+    config.generators do |g|
+      g.assets false
+      g.skip_routes false
+      g.test_framework false
+      g.helper false
+    end
     config.generators.system_tests = nil
   end
 end
